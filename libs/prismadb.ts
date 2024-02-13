47b4d493724d3e30450172f57e16f08af89649a0
Aaryan Kalra <aryan.smart832@gmail.com>
2024-02-13 21:24:58 +0530

    as

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,11 +1,26 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// const prismadb = globalThis.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+// export default prismadb;
 import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+const prismadb = globalThis.prisma ?? prismaClientSingleton();
 
 export default prismadb;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
