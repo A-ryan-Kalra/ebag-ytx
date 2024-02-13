a04b79b76030594df78e142df0fdc8e7c2c767ef
Aaryan Kalra <aryan.smart832@gmail.com>
2024-02-13 23:09:20 +0530

    changes

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -6,9 +6,9 @@ declare global {
 
 const prismadb = globalThis.prisma || new PrismaClient();
 
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
-export default prismadb;
+if (process.env.NODE_ENV === "production") globalThis.prisma = prismadb;
 
+export default prismadb;
 // import { PrismaClient } from "@prisma/client";
 
 // const prismaClientSingleton = () => {
