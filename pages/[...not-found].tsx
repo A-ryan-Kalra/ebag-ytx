27b0c34c85381b1215112cf0a661024cccbfcd8e
Aaryan Kalra <aryan.smart832@gmail.com>
2024-02-27 02:20:59 +0530

    as

---
+++ pages/[...not-found].tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex   justify-center flex-col gap-3 items-center h-[90vh]">
+      <div className="w-32 h-32 relative">
+        <Image alt="error" src={"/img/link.svg" || ""} fill />
+      </div>
+      <p className="text-[20px]">Could not find requested link.</p>
+      <Link
+        href="/"
+        className="bg-[#0765FC] text-white p-2 rounded-full shadow-md"
+      >
+        Go back home
+      </Link>
+    </div>
+  );
+}
