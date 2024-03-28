49ebc8dd4a25c31e5bc3144338c3945f0765f48d
Aaryan Kalra <aryan.smart832@gmail.com>
2024-03-28 21:37:00 +0530

    add tooltip

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,6 +11,7 @@ import Cart from "./Cart/Cart";
 import { hideScrollbar, showScrollbar } from "../constants/hidescrollbar";
 import PWAModal from "./PWAModal";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { Tooltip } from "@nextui-org/react";
 
 function Layout({ children }: { children: ReactNode }) {
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -31,8 +32,14 @@ function Layout({ children }: { children: ReactNode }) {
       prompt.userChoice.then((choiceResult: any) => {
         if (choiceResult.outcome === "accepted") {
           console.log("Accepted");
+          showScrollbar();
+          setShowModal(false);
+          setPwa(false);
         } else {
           console.log("Cancelled");
+          setShowModal(false);
+          setPwa(false);
+          showScrollbar();
         }
         setPrompt(null);
         setShowModal(false);
@@ -71,17 +78,24 @@ function Layout({ children }: { children: ReactNode }) {
   return (
     <div className={`relative`}>
       {showModal && (
-        <div
-          onClick={showPwa}
-          className="fixed bottom-20 cursor-pointer hover:shadow-slate-500 shadow-md duration-300 ease-in-out z-[100] bg-slate-400 py-3 px-3 right-10 rounded-full"
+        <Tooltip
+          showArrow={true}
+          content="Install App"
+          color="primary"
+          className="bg-blue-500 rounded-full text-white p-2"
         >
-          <Icon
-            icon="tdesign:device"
-            width={30}
-            height={30}
-            className="text-white "
-          />
-        </div>
+          <div
+            onClick={showPwa}
+            className="fixed bottom-20 cursor-pointer hover:shadow-slate-500 shadow-md duration-300 ease-in-out z-[100] bg-slate-400 py-3 px-3 right-10 rounded-full"
+          >
+            <Icon
+              icon="tdesign:device"
+              width={30}
+              height={30}
+              className="text-white "
+            />
+          </div>
+        </Tooltip>
       )}
       {pwa && (
         <PWAModal
